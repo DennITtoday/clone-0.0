ddf35a5a6215c472aac0011c1b6e9ccd6aeea3cb
DennITtoday <99242289+DennITtoday@users.noreply.github.com>
2022-07-15 04:33:26 +0300

    committer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { AppProps } from "next/app";
+import MainLayout from "../components/MainLayout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const colors = {
+    brand: {
+      50: "#ecefff",
+      100: "#cbceeb",
+      200: "#a9aed6",
+      300: "#888ec5",
+      400: "#666db3",
+      500: "#4d5499",
+      600: "#3c4178",
+      700: "#2a2f57",
+      800: "#181c37",
+      900: "#080819",
+    },
+  };
+
+  const config = { initialColorMode: "dark", useSystemColorMode: false };
+
+  const theme = extendTheme({ colors, config });
+  return (
+    <ChakraProvider theme={theme}>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
